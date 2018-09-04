96df56cf844e03710b9c8471e5ba097a6f753ca5
Ewokinator <whatacrap@gmail.com>
2018-09-05 00:13:48 +0300

    Детальная стилизация 5

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
 var contactsButton = document.querySelector(".contacts-button");
+
 var modalFeedback = document.querySelector(".modal-feedback");
+
 var modalClose = document.querySelector(".modal-close");
+
 var modalOverlay = document.querySelector(".modal-overlay");
+
+var feedbackName = modalFeedback.querySelector("[name=feedback-name]");
+var feedbackMail = modalFeedback.querySelector("[name=feedback-mail]");
+var feedbackMessage = modalFeedback.querySelector("[name=feedback-message]");
+
+var form = modalFeedback.querySelector("form");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("feedbackName");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 contactsButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFeedback.classList.add("modal-show-bounce");
   modalOverlay.classList.add("modal-show");
+
+  if (storage) {
+    feedbackName.value = storage;
+    feedbackMail.focus();
+  }
+  else {
+    feedbackName.focus();
+  }
 });
 
 modalClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalFeedback.classList.remove("modal-show-bounce");
   modalOverlay.classList.remove("modal-show");
+  modalFeedback.classList.remove("modal-error");
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -21,9 +48,26 @@ window.addEventListener("keydown", function (evt) {
       evt.preventDefault();
       modalFeedback.classList.remove("modal-show-bounce");
       modalOverlay.classList.remove("modal-show");
+      modalFeedback.classList.remove("modal-error");
     }
   }
 });
+
+form.addEventListener("submit", function (evt) {
+  if (!feedbackName.value || !feedbackMail.value || !feedbackMessage.value) {
+    evt.preventDefault();
+    modalFeedback.classList.add("modal-error");
+  }
+  else {
+    if (isStorageSupport) {
+      localStorage.setItem("feedbackName", feedbackName.value);
+    }
+  }
+
+})
+
+
+
 
 ymaps.ready(init);
 function init(){
