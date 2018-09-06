ca1528dd5c04c2fac1c44522a55c8152e2c90e62
Ewokinator <whatacrap@gmail.com>
2018-09-06 23:03:48 +0300

    Оформление контента 3

--- js/script.js
+++ js/script.js
@@ -69,18 +69,39 @@ form.addEventListener("submit", function (evt) {
 
 
 
-ymaps.ready(init);
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.939519, 30.329234],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
 
-function init(){
-  var myMap = new ymaps.Map("map", {
-  center: [59.9, 30.3],
-  zoom: 14
-  });
-}
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
 
-var myPlacemark = new ymaps.Placemark([59.9, 30.3], {
-  hintContent: 'Содержимое всплывающей подсказки',
-  balloonContent: 'Содержимое балуна'
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: '',
+            iconImageSize: [80, 104],
+            iconImageOffset: [-30, -68]
+        }),
+
+        myPlacemarkWithContent = new ymaps.Placemark([59.938631, 30.323055], {
+
+        }, {
+            iconLayout: 'default#imageWithContent',
+            iconImageHref: 'img/pin.svg',
+            iconImageSize: [80, 140],
+            iconImageOffset: [-45, -130],
+            iconContentOffset: [0, 0],
+            iconContentLayout: MyIconContentLayout
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
 });
-
-myMap.geoObjects.add(myPlacemark);
